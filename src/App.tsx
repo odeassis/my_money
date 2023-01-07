8d4dd2247db9efecfe79714d171a32227c495720
Francisco de Assis <franciscodeassisc.santos@gmail.com>
2023-01-06 22:24:32 -0300

    refactor: add Transaction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/Theme/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Transactions />
       <GlobalStyle />
     </ThemeProvider>
   );
